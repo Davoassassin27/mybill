074e93330d6b6f4bd15faac63fbed8f2c2135570
Davoassassin27 <davosoler@gmail.com>
2026-02-04 13:32:53 -0300

    fix mvp deply pb

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,6 +2,8 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
